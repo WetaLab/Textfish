bb6794d8c1966916f703dca910e720ae832b939f
Static <static.wetalabs@outlook.com>
2025-08-14 15:00:11 +0200

    remove "." from image descriptions

--- src/Textfish/analyzer.js
+++ src/Textfish/analyzer.js
@@ -212,7 +212,12 @@ export async function describeImage(url) {
       contents,
     });
 
-    return `[${result.candidates[0]?.content.parts[0].text || ""}]`;
+    var text = result.candidates[0]?.content.parts[0].text || "";
+    if (text.endsWith(".")) {
+      text = text.slice(0, -1); // Remove the last character
+    }
+
+    return `[${text}]`;
   } catch (e) {
     console.error("Error generating image description:", e);
     return "";
